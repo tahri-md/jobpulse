2b7b3a8940d5778fd89e34c8d75b2c16c1c3fab8
tahri-md <tahrimedamine00@gmail.com>
2026-06-24 20:41:17 +0100

    fix(auth): add password reset token repository

---
+++ jobpulse/src/main/java/com/jobpulse/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,11 @@
+package com.jobpulse.repository;
+
+import com.jobpulse.model.PasswordResetToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+  Optional<PasswordResetToken> findByToken(String token);
+}
